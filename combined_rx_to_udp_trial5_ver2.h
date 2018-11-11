eaf019c61ed5c1d544453fa6055c2ef9f8911b65
zcapbie <33893212+zcapbie@users.noreply.github.com>
2018-11-10 20:51:55 -0800

    Update combined_rx_to_udp_trial5_ver2.h
    Added Comments

--- combined_rx_to_udp_trial5_ver2.h
+++ combined_rx_to_udp_trial5_ver2.h
@@ -3,7 +3,7 @@
 // Copyright 2018 Ettus Research, a National Instruments Company
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
-//
+
 //Modified by Nicole Bienert
 
 #include <uhd/types/tune_request.hpp>
